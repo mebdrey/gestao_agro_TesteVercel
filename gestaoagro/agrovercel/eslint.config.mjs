96cf33eb1283963b45d37eb64e299efb23ac5ca4
mebdrey <mebdelrey@gmail.com>
2025-09-18 14:39:07 -0300

    seilaaaa

--- gestaoagro/agrovercel/eslint.config.mjs
+++ gestaoagro/agrovercel/eslint.config.mjs
@@ -5,11 +5,21 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({baseDirectory: __dirname,});
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals"),
-  {ignores: ["node_modules/**", ".next/**","out/**","build/**","next-env.d.ts",],},
+  {
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+    ],
+  },
 ];
 
 export default eslintConfig;
